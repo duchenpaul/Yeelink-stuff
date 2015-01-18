8abd605bde21c9c82fd532fa5e545ea1ae4f401a
duchenpaul <duchenpaul@gmail.com>
2015-01-18 22:32:01 +0800

    过滤掉1970年无效时间，等待校时完成

--- router_yeelink.sh
+++ router_yeelink.sh
@@ -5,6 +5,16 @@ echo $CURTIME "Yeelink started" >>/data/usr/log/yeelink.log
 
 sleep 30
 while [ 1 ];do
+
+#Time check
+YEAR_CHK=`date |cut -d ' ' -f 6`
+while [ $YEAR_CHK -lt 2014 ]; do
+	echo ''$CURTIME', time is wrong,wait for 5 sec...'>>/tmp/log/yeelink.log
+	#ntpdate 192.168.31.1
+	sleep 5
+	YEAR_CHK=`date |cut -d ' ' -f 6`
+done
+
 CURTIME=`date +"%Y-%m-%dT%H:%M:%S"`
 PCBTEMP=`/usr/sbin/readtmp | /usr/bin/awk '{print $2}'`
 DISKTEMP=`/usr/sbin/smartctl -A /dev/sda | grep Temperature_Celsius | /usr/bin/awk '{print $10}'`
@@ -16,17 +26,17 @@ echo $CURTIME $PCBTEMP $DISKTEMP $FANSPEED $LOADAVG >>/tmp/log/yeelink.log
 if [ $PCBTEMP -ge 0 ]
 then
 echo '{"timestamp":"'$CURTIME'", "value":'$PCBTEMP'}' >/tmp/datafile
-/usr/bin/wget -q --post-file=/tmp/datafile --header="U-ApiKey:86493543ff87c604bc56fac6a89aee56" -O /tmp/yeelink http://api.yeelink.net/v1.0/device/15031/sensor/30299/datapoints
+/usr/bin/wget -q --post-file=/tmp/datafile --header="U-ApiKey:86493543ff87c604bc56fac6a89aee56" -O /tmp/yeelink http://api.yeelink.net/v1.0/device/15031/sensor/30320/datapoints
 fi
 
 echo '{"timestamp":"'$CURTIME'", "value":'$DISKTEMP'}' >/tmp/datafile	
-/usr/bin/wget -q --post-file=/tmp/datafile --header="U-ApiKey:86493543ff87c604bc56fac6a89aee56" -O /tmp/yeelink http://api.yeelink.net/v1.0/device/15031/sensor/30300/datapoints
+/usr/bin/wget -q --post-file=/tmp/datafile --header="U-ApiKey:86493543ff87c604bc56fac6a89aee56" -O /tmp/yeelink http://api.yeelink.net/v1.0/device/15031/sensor/30321/datapoints
 
 echo '{"timestamp":"'$CURTIME'", "value":'$FANSPEED'}' >/tmp/datafile
-/usr/bin/wget -q --post-file=/tmp/datafile --header="U-ApiKey:86493543ff87c604bc56fac6a89aee56" -O /tmp/yeelink http://api.yeelink.net/v1.0/device/15031/sensor/30301/datapoints
+/usr/bin/wget -q --post-file=/tmp/datafile --header="U-ApiKey:86493543ff87c604bc56fac6a89aee56" -O /tmp/yeelink http://api.yeelink.net/v1.0/device/15031/sensor/30322/datapoints
 
 echo '{"timestamp":"'$CURTIME'", "value":'$LOADAVG'}' >/tmp/datafile
-/usr/bin/wget -q --post-file=/tmp/datafile --header="U-ApiKey:86493543ff87c604bc56fac6a89aee56" -O /tmp/yeelink http://api.yeelink.net/v1.0/device/15031/sensor/30302/datapoints
+/usr/bin/wget -q --post-file=/tmp/datafile --header="U-ApiKey:86493543ff87c604bc56fac6a89aee56" -O /tmp/yeelink http://api.yeelink.net/v1.0/device/15031/sensor/30323/datapoints
 
 sleep 59
 done
